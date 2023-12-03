1390083c31824a6fd3490ba2b87c8cd14c8271a1
Maciej Treder <treder.mc@gmail.com>
2023-12-03 16:36:21 +0100

    genetic algorithm

--- main.js
+++ main.js
@@ -2,24 +2,35 @@ const canvas=document.getElementById("myCanvas");
 canvas.width=600;
 
 const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2, canvas.width*0.9);
+const road = new Road(canvas.width/2, canvas.width*0.5);
 
 //const ship = new Ship(road.getLaneCenter(2), 100, 30, 50, "AI");
 const N = 100;
 const ships = generateShips(N);
 let bestOne = ships[0];
 if (localStorage.getItem("bestBrain")) {
-    bestOne.brain = JSON.parse(
-        localStorage.getItem("bestBrain")
-    );
+    for(let i=0; i < ships.length; i++) {
+        ships[i].brain = JSON.parse(
+            localStorage.getItem("bestBrain")
+        );
+        if(i!=0) {
+            NeuralNetwork.mutate(ships[i].brain, 0.2);
+        }
+    }
+   
 }
 
 const traffic = [
     new Ship(road.getLaneCenter(2),-100,30,50, "DUMMY", 2),
-    new Ship(road.getLaneCenter(1),-200,30,50, "DUMMY", 2),
-    new Ship(road.getLaneCenter(0),-200,30,50, "DUMMY", 2),
-    new Ship(road.getLaneCenter(4),-200,30,50, "DUMMY", 2),
-    new Ship(road.getLaneCenter(3),-250,30,50, "DUMMY", 2)
+    new Ship(road.getLaneCenter(1),-400,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(0),-400,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(4),-700,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(3),-700,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(4),-1000,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(2),-1000,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(1),-1300,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(3),-1300,30,50, "DUMMY", 2),
+    new Ship(road.getLaneCenter(4),-1300,30,50, "DUMMY", 2),
 ];
 
 animate();
